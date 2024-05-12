eeb8a4fb7e0a988425159ee149f82223895f6ee1
Talika Bajaj <bajajtalika2001@gmail.com>
2024-05-12 14:57:40 +0530

    Add files

---
+++ README.md
@@ -0,0 +1,16 @@
+# Frontend Mentor - Intro component with sign up form
+
+![Design preview for the Intro component with sign up form coding challenge](./design/desktop-preview.jpg)
+
+## Hi! 👋
+
+This is my 21st Frontend Mentor Coding Challenge.
+
+In this challenge, I developed a sign-up form using HTML, CSS, and JavaScript, designed to enhance user experience and interaction. The form captures essential details like first name, last name, email address, and password. To ensure data integrity, I implemented client-side validation, where if any input field is left empty, a message dynamically appears next to the respective field upon clicking the submit button, prompting the user to fill it out.
+
+Adding a touch of personalization, I incorporated animated labels for each input field. These labels smoothly transition when clicked, elegantly moving above the input field to provide context. If the input field is left empty, the label gracefully animates back to its original position, maintaining a polished user interface.
+
+Furthermore, I introduced a 'show password' feature for enhanced security and convenience. By clicking an eye icon adjacent to the password field, users can toggle between revealing and hiding their password. The icon dynamically updates its appearance, cleverly displaying a slash when the password is visible, providing users with clear feedback and control over their input.
+
+
+**Had fun building!** 🚀
---
+++ script.js
@@ -0,0 +1,113 @@
+fname.addEventListener("focus", () => {
+    document.querySelector('.fName').classList.add('active');
+});
+fname.addEventListener("focusout", () => {
+    if (fname.value === "") {
+        document.querySelector('.fName').classList.remove('active');
+    }
+});
+
+
+lname.addEventListener("focus", () => {
+    document.querySelector('.lName').classList.add('active');
+});
+lname.addEventListener("focusout", () => {
+    if (lname.value === "") {
+        document.querySelector('.lName').classList.remove('active');
+    }
+});
+
+
+email.addEventListener("focus", () => {
+    document.querySelector('.email').classList.add('active');
+});
+email.addEventListener("focusout", () => {
+    if (email.value === "") {
+        document.querySelector('.email').classList.remove('active');
+    }
+});
+
+
+pwd.addEventListener("focus", () => {
+    document.querySelector('.pass').classList.add('active');
+});
+pwd.addEventListener("focusout", () => {
+    if (pwd.value === "") {
+        document.querySelector('.pass').classList.remove('active');
+    }
+});
+
+
+function validateEmail() {
+    const format = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email.value === "") {
+        document.getElementById('error-em').style.display = 'block';
+        document.getElementById('error-email').style.display = 'block';
+        document.getElementById('email').style.borderColor = 'hsl(0, 100%, 74%)';
+    } else if (email.value.match(format)) {
+        document.getElementById('email').value = '';
+        document.getElementById('fname').value = '';
+        document.getElementById('lname').value = '';
+        document.getElementById('pwd').value = '';
+        document.getElementById('error-msg').style.display = 'none';
+        offFocus();
+    } else {
+        document.getElementById('error-msg').style.display = 'block';
+        document.getElementById('email').style.borderColor = 'hsl(0, 100%, 74%)';
+    }
+}
+
+const trialBtn = document.getElementById('trial-btn');
+
+trialBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const inputs = document.getElementsByClassName('inputs');
+    const errors = document.getElementsByClassName('error');
+    const errorIcons = document.getElementsByClassName('error-icon');
+    // const errorMsg = document.getElementById('error-msg');
+
+    for (let index = 0; index < inputs.length; index++) {
+        const element = inputs[index];
+        // console.log(element);
+        if (element.value === '') {
+            errors[index].style.display = 'block';
+            errorIcons[index].style.display = 'block';
+            element.style.borderColor = 'hsl(0, 100%, 74%)';
+
+        } else {
+            errors[index].style.display = 'none';
+            errorIcons[index].style.display = 'none';
+            element.style.borderColor = 'hsl(246, 25%, 77%)';
+        }
+    }
+
+    validateEmail();
+})
+
+
+function offFocus() {
+    const labels = document.getElementsByClassName('all-labels');
+    const inputs = document.getElementsByClassName('inputs');
+    for (let index = 0; index < inputs.length; index++) {
+        const element = inputs[index];
+        // const input = inputs[index];
+        console.log(element.value);
+        if (element.value == '') {
+            labels[index].classList.remove('active');
+            console.log('empty')
+        } else {
+            labels[index].classList.add('active');
+        }
+    }
+}
+
+const togglePassword = document.getElementById('toggle-password');
+const passField = document.getElementById('pwd');
+togglePassword.addEventListener('click', () => {
+    const type = passField.getAttribute('type') === 'password' ? 'text' : 'password';
+    passField.setAttribute('type', type);
+})
+
+function myFunction(x) {
+    x.classList.toggle("bi-eye-slash-fill");
+}
